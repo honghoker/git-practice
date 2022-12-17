d25e7ee1dbfe8dec370ac3cb36a1084a864f4c6b
honghoker <ekflzhftk@naver.com>
2022-12-17 17:20:29 +0900

    Replace Lions with Leopards

---
+++ git-practice/git-practice/leopards.swift
@@ -0,0 +1,17 @@
+//
+//  leopards.swift
+//  git-practice
+//
+//  Created by 홍은표 on 2022/12/17.
+//
+
+import Foundation
+
+//team: Leopards
+//
+//manager: Luke
+//
+//members:
+//- Linda
+//- William
+//- David
