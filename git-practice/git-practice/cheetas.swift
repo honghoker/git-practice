8df1621a1d78b1019b5795d071d042db3bdb872b
honghoker <ekflzhftk@naver.com>
2022-12-17 17:25:07 +0900

    Add team Cheetas

---
+++ git-practice/git-practice/cheetas.swift
@@ -0,0 +1,17 @@
+//
+//  cheetas.swift
+//  git-practice
+//
+//  Created by 홍은표 on 2022/12/17.
+//
+
+import Foundation
+
+//team: Cheetas
+//
+//manager: Laura
+//
+//members:
+//- Ryan
+//- Anna
+//- Justin
