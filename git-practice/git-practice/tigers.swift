420c676256abc254ac39c49108b74ed3f43a2682
honghoker <ekflzhftk@naver.com>
2022-12-17 17:21:27 +0900

    Add George to Tigers

---
+++ git-practice/git-practice/tigers.swift
@@ -0,0 +1,18 @@
+//
+//  tigers.swift
+//  git-practice
+//
+//  Created by 홍은표 on 2022/12/17.
+//
+
+import Foundation
+
+//team: tigers
+//
+//manager: Luke
+//
+//members:
+//- Linda
+//- William
+//- David
+//- George
